9b6aa4665001f13e0509a565f740b2e4ac383df3
ebkeefe <ebkeefe@willamette.edu>
2022-10-22 14:30:50 -0700

    clean up board dimension

--- game.js
+++ game.js
@@ -6,20 +6,18 @@ module.exports = initBoard;
 const{ BOARD_DIMENSION } = require('./constants');
 
 function initBoard() {
-  boardDimension = 9;
   const board = createBoard()
   return board;
 }
 
 function createBoard() {
-  console.log(BOARD_DIMENSION);
   board = []
   for (var i = 0 ; i < BOARD_DIMENSION; i++)
   {
     row = new Array(BOARD_DIMENSION).fill(0);
     board.push(row);
   }
-   
+
   board[(BOARD_DIMENSION-1)/2][(BOARD_DIMENSION-1)/2]=-1;
 
   return board;
